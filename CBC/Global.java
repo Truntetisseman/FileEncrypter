bb692448d0c38a9eed0e3f449f08ebf67e97eb61
Leon <noelem@protonmail.com>
2022-04-27 23:29:21 +0200

    removed comment

--- CBC/Global.java
+++ CBC/Global.java
@@ -4,16 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface Global {
-    //Paths and other global variables
-  /*  String path = "/Users/danielnoren/Desktop"; ///Users/noel/Desktop
-    String fileName = "MedicalRecordNielsJ"; //
-    String fileSuffix = "pdf"; // eg. pdf, jpeg, docx
-
-    //Variables for decrypt
-    String originalSHAFile = Global.path + "/" + Global.fileName + "." + Global.fileSuffix; //"MedicalRecordNielsJ.pdf" ;
-    String testFile = Global.path + "/" + Global.fileName + "." + "test" + "." + Global.fileSuffix; /* originalSHAFile;
-    String plaintextFileName = Global.path + "/" + Global.fileName + "." + Global.fileSuffix + "." + "aes";// + testFile;
-    */
 
     //Keystore
     String keystoreDir = "/Users/patrickorum/Desktop/testfolder/keystore";
