93f1f09ddd25074ba9d0cd53f4294b24d31ff21f
CloudProDk <59182385+CloudProDk@users.noreply.github.com>
2022-04-28 21:26:44 +0200

    Updated to main

--- CBC/Views/Main.java
+++ CBC/Views/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ViewManager extends Application {
+public class Main extends Application {
 
     static Stage stages = new Stage();
 
